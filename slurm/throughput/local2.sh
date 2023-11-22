3ff9601e7d233308df99a075b63c3ced8a478554
luosuu <zhluosuu@outlook.com>
2023-11-22 00:11:05 -0500

    update for local test

--- slurm/throughput/local2.sh
+++ slurm/throughput/local2.sh
@@ -9,8 +9,8 @@
 #SBATCH -A bii_dsc_community
 #SBATCH --time=04:00:00          # total run time limit (HH:MM:SS)
 #SBATCH --reservation=bi_fox_dgx
-#SBATCH --error="slurm/throughput/8nA100_throughput3.err"
-#SBATCH --output="slurm/throughput/8nA100_throughput3.output"
+#SBATCH --error="slurm/throughput/local2.err"
+#SBATCH --output="slurm/throughput/local2.output"
 
 export MASTER_PORT=$(expr 10000 + $(echo -n $SLURM_JOBID | tail -c 4))
 export WORLD_SIZE=$(($SLURM_NNODES * $SLURM_NTASKS_PER_NODE))
@@ -30,18 +30,21 @@ cd /scratch/fad3ew/rtp
 SCRIPTS=(
 multi_dp_benchmark.py
 multi_fsdp_benchmark.py
+multi_tp_benchmark.py
 multi_rtp_benchmark.py
 multi_rtp_benchmark_inplace.py
 )
 
+
 CONFIGS=(
+gpt2-large
 EleutherAI_gpt-neo-1.3B
 )
 
 
 for config in "${CONFIGS[@]}"; do
     for script in "${SCRIPTS[@]}"; do
-        for i in {1..14}; do
+        for i in {1..1}; do
             srun --export=ALL /scratch/fad3ew/rtp/.venv/bin/python \
             benchmarks/$script \
             --use_synthetic_data \
